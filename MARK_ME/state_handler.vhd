668bb521902731edea24a29f77b7a46c1320e0fb
Merle Dechering <mde121@uclive.ac.nz>
2022-05-27 10:15:53 +0000

    Upload New File

---
+++ MARK_ME/state_handler.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.05.2022 15:20:15
+-- Design Name: 
+-- Module Name: state_handler - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity state_handler is
+--  Port ( );
+end state_handler;
+
+architecture Behavioral of state_handler is
+
+begin
+
+
+end Behavioral;
